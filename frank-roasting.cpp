c743e7d99f47722c3251f128a740488e5b127bac
frankjin5 <138106205+frankjin5@users.noreply.github.com>
2025-08-15 19:01:39 +1200

    Added closing line, added newline between stanzas

--- frank-roasting.cpp
+++ frank-roasting.cpp
@@ -3,19 +3,25 @@
 
 using namespace std;
 
-string roast_frank(int number_of_times) {
+string roast_frank(const int number_of_times) {
+    int times_left = number_of_times;
     string poem;
 
-    while (number_of_times > 0) {
+    while (times_left > 0) {
         poem += "Imagine not knowing how to use OOP\n";
         poem += "Imagine not knowing how C++ modules work\n";
         poem += "Imagine not knowing Lua\n";
         poem += "\n";
         poem += "HAHAHAHA!\n";
 
-        --number_of_times;
-        poem += "I'LL ROAST YOU " + to_string(number_of_times) + " MORE TIMES!\n";
-    }
+        --times_left;
 
+        if (times_left == 0) {
+                poem += "I'VE JUST ROASTED YOU " + to_string(number_of_times) + " TIMES!\n";
+        } else {
+            poem += "I'LL ROAST YOU " + to_string(times_left) + " MORE TIMES!\n";
+        }
+        poem += "\n";
+    }
     return poem;
 }
